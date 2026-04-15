61953e7eda5e2c0e4134b918df9165f8ba29aa02
qfjz <red@qfj.pl>
2026-04-15 07:39:15 +0200

    2026-04-15-07:39:13

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -21,3 +21,35 @@ autocmd("FileType", {
         vim.opt_local.signcolumn = "no"
     end,
 })
+
+autocmd('TermOpen', {
+    group = augroup('startinsert', {}),
+    callback = function()
+        vim.cmd.startinsert()
+    end,
+})
+
+-- zapisanie pozycji kursora
+autocmd('BufReadPost', {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+
+autocmd('FileType', {
+    group = augroup('close_with_q', {}),
+    pattern = {
+        'scratch',
+        'help',
+        'qf',
+        'nvim-undotree'
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set('n', 'q', '<cmd>bd!<cr>', { nowait = true, buffer = event.buf, silent = true })
+    end,
+})
