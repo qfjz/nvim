4d414efff8cdc23146b99af68871db23dde0a142
qfjz <red@qfj.pl>
2026-05-27 18:21:00 +0200

    2026-05-27-18:20:58

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -2,6 +2,32 @@ require("luasnip").config.setup({
     history = true,              -- Zapamiętuj historię snippetów
     updateevents = "TextChanged,TextChangedI", -- Aktualizuj przy zmianie tekstu
     enable_autosnippets = true, -- Włącz automatyczne snippety (opcjonalnie)
+    ext_opts = {
+        [require("luasnip.util.types").insertNode] = {
+            unvisited = {
+                -- Opcja 1: Wirtualny tekst
+                virt_text = { { "⤚", "WarningMsg" } },
+                virt_text_pos = "inline",
+                -- Opcja 2: Podświetlenie tła (działa tylko jeśli pozycja ma tekst)
+                hl_group = "Visual",  -- lub "IncSearch" dla mocniejszego efektu
+            },
+            visited = {
+                hl_group = "Comment",
+            },
+        },
+        [require("luasnip.util.types").exitNode] = {
+            unvisited = {
+                virt_text = { { "◼", "Identifier" } },
+                virt_text_pos = "inline",
+            },
+        },
+        [require("luasnip.util.types").choiceNode] = {
+            active = {
+                virt_text = { { "▼", "Special" } },
+                virt_text_pos = "inline",
+            },
+        },
+    },
 })
 -- Załaduj snippety z friendly-snippets
 require("luasnip.loaders.from_vscode").lazy_load()
